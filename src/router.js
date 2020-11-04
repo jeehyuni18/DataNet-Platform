194c9f3435942f6e8de098a6383e860a9e408389
jeehyuni18 <jeehyuni18@illunex.com>
2020-11-04 13:46:26 +0900

    최종 커밋

--- src/router.js
+++ src/router.js
@@ -18,51 +18,55 @@ export default function MyRouter() {
     return (
         <Router>
             <Layout style={{ minHeight: '100vh' }}>
-                <Sider width={300}>
-                    <Link to={"/"}>
-                        <div style={{
-                            height: '32px',
-                            background: 'rgba(255, 255, 255, 0.2)',
-                            margin: '16px'
-                        }} />
-                    </Link>
-                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                        <Menu.Item key="2" >
-                            <Link to={"/board"}>
-                                데이터 입력
-                            </Link>
-                        </Menu.Item>
-                        <SubMenu title={"Data-Network Visualizaion"}>
-                            <Menu.Item>
-                                <Link to={"/graph"}>
-                                    Data-Analytics Graph
+                <Header className="site-layout-background" style={{ padding: 0 }} >
+                    <div style={{color:'#fff',textAlign:'center',fontSize:'30px',fontWeight:'500'}}>
+                        DATANETWORK PLATFORM
+                    </div>
+                </Header>
+                <Layout>
+                    <Sider width={300}>
+                        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                            <Menu.Item key="1" >
+                                <Link to={"/board"}>
+                                    Main
                                 </Link>
                             </Menu.Item>
-                        </SubMenu>
-                    </Menu>
-                </Sider>
-                <Layout className="site-layout">
-                    <Header className="site-layout-background" style={{ padding: 0 }} />
-                    <Content style={{ margin: '0 16px' }}>
-                        <Switch>
-                            <Route exact path="/">
-                                <iframe width="100%" height="100%" src="http://gvc.effectmall.com/gvc"/>
-                            </Route>
-                            <Route exact path="/board">
-                                <Board />
-                            </Route>
-                            <Route path="/chart">
-                                <Chart />
-                            </Route>
-                            <Route path="/graph">
-                                <Graph />
-                            </Route>
-                            <Route path="/register">
-                                <Register />
-                            </Route>
-                        </Switch>
-                    </Content>
-                    <Footer style={{ textAlign: 'center' }}>jihyun_kang © 2020 Create</Footer>
+                            <Menu.Item key="2" >
+                                <Link to={"/board"}>
+                                    데이터 입력
+                                </Link>
+                            </Menu.Item>
+                            <SubMenu title={"Data-Network Visualizaion"}>
+                                <Menu.Item>
+                                    <Link to={"/graph"}>
+                                        Data-Analytics Graph
+                                    </Link>
+                                </Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
+                    <Layout className="site-layout">
+                        <Content style={{ margin: '0 16px' }}>
+                            <Switch>
+                                <Route exact path="/">
+                                    <iframe width="100%" height="100%" src="http://gvc.effectmall.com/gvc"/>
+                                </Route>
+                                <Route exact path="/board">
+                                    <Board />
+                                </Route>
+                                <Route path="/chart">
+                                    <Chart />
+                                </Route>
+                                <Route path="/graph">
+                                    <Graph />
+                                </Route>
+                                <Route path="/register">
+                                    <Register />
+                                </Route>
+                            </Switch>
+                        </Content>
+                        <Footer style={{ textAlign: 'center' }}>jihyun_kang © 2020 Create</Footer>
+                    </Layout>
                 </Layout>
             </Layout>
         </Router>
